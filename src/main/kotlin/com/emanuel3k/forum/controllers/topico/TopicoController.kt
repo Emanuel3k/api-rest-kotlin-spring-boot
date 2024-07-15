438352f1a7c921b6c15606c3e12103f2a1323f61
Emanuel3k <emanuelsantos_677@hotmail.com>
2024-07-15 15:26:14 -0300

    feat: bean validation

--- src/main/kotlin/com/emanuel3k/forum/controllers/topico/TopicoController.kt
+++ src/main/kotlin/com/emanuel3k/forum/controllers/topico/TopicoController.kt
@@ -3,6 +3,7 @@ package com.emanuel3k.forum.controllers.topico
 import com.emanuel3k.forum.domain.dto.topico.NovoTopicoForm
 import com.emanuel3k.forum.domain.dto.topico.TopicoView
 import com.emanuel3k.forum.services.topico.TopicoService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -17,7 +18,7 @@ class TopicoController(
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody dto: NovoTopicoForm) {
+    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
         service.cadastrar(dto)
     }
 
--- src/main/kotlin/com/emanuel3k/forum/domain/dto/topico/NovoTopicoDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.emanuel3k.forum.domain.dto.topico
-
-data class NovoTopicoForm(
-    val titulo: String,
-    val mensagem: String,
-    val idCurso: Long,
-    val idAutor: Long,
-)
